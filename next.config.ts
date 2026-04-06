2aed840ed556e7affce1c9e6b1e4787514bb5b3f
creatureofthefeature <eldwretchtheeldritch@gmail.com>
2026-04-05 22:41:58 -0500

    gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = { 
+  basePath: isProd ? '/personal-website' : '', 
+  assetPrefix: isProd ? '/personal-website/' : '', 
+  images: { unoptimized: true, }, 
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
