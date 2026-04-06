42fc2387711cfcbceba443d7c30fa2bbca4389e2
creatureofthefeature <eldwretchtheeldritch@gmail.com>
2026-04-05 22:10:45 -0500

    basepath and assetprefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/personal-website',
+  assetPrefix: '/personal-website/',
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
